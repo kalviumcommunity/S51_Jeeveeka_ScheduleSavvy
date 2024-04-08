628b281675866601e55065a8d7c6c4b8752d3c09
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-04-08 10:55:05 +0530

    changes made

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -7,15 +7,15 @@ export default function Navbar() {
     <>
         <nav>
             <div className="wrapper">
-                <div className="logo"><a href="#">Logo</a></div>
+                <div className="logo"><h6>Logo</h6></div>
                 <input type="radio" name="slider" id="menu-btn"/>
                 <input type="radio" name="slider" id="close-btn"/>
                 <ul className="nav-links">
                 <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times"></i></label>
-                <li><a href="#">Calender</a></li>
-                <li><a href="#">Myself</a></li>
+                <li><h6>Calender</h6></li>
+                <li><h6>Myself</h6></li>
                 <li>
-                    <a href="#" className="desktop-item">Features</a>
+                    <h6 className="desktop-item">Features</h6>
                     <input type="checkbox" id="showMega"/>
                     <label htmlFor="showMega" className="mobile-item">Features</label>
                     <div className="mega-box">
@@ -26,33 +26,33 @@ export default function Navbar() {
                         <div className="row">
                         <header>All-In-One Organizer</header>
                         <ul className="mega-links">
-                            <li><a href="#">Social Media Dashboard</a></li>
-                            <li><a href="#">Timer</a></li>
-                            <li><a href="#">Challenges</a></li>
-                            <li><a href="#">Motivation</a></li>
+                            <li><h6>Social Media Dashboard</h6></li>
+                            <li><h6>Timer</h6></li>
+                            <li><h6>Challenges</h6></li>
+                            <li><h6>Motivation</h6></li>
                         </ul>
                         </div>
                         <div className="row">
                         <header>Creative Customization</header>
                         <ul className="mega-links">
-                            <li><a href="#">Themes</a></li>
-                            <li><a href="#">Music Library</a></li>
+                            <li><h6>Themes</h6></li>
+                            <li><h6>Music Library</h6></li>
                         </ul>
                         </div>
                     </div>
                     </div>
                 </li>
                 <li>
-                    <a href="#" className="desktop-item">More Info</a>
+                    <h6 className="desktop-item">More Info</h6>
                     <input type="checkbox" id="showDrop"/>
                     <label htmlFor="showDrop" className="mobile-item">More Info</label>
                     <ul className="drop-menu">
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Feedback</a></li>
+                    <li><h6>About Us</h6></li>
+                    <li><h6>Contact Us</h6></li>
+                    <li><h6>Feedback</h6></li>
                     </ul>
                 </li>
-                <li><a href="#">Profile</a></li>
+                <li><h6>Profile</h6></li>
                 </ul>
                 <label htmlFor="menu-btn" className="btn menu-btn"><i className="fas fa-bars"></i></label>
             </div>
