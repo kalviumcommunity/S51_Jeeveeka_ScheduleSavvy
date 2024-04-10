57b06d9378a41dede30d6f8ad3b283543a0189f1
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-04-10 12:08:23 +0530

    made changes according to review

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import Loader from './Loader'
 
 export default function Home() {
-  window.addEventListener('load', function() {
-    document.getElementById('main_content').style.display = 'none';
-    setTimeout(function() {
-        document.getElementById('loader_container').style.display = 'none';
-        document.getElementById('main_content').style.display = 'block';
-    }, 3000);
-});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      const timeout = setTimeout(() => {
+          setIsLoading(false);
+      }, 4000); 
+      return () => clearTimeout(timeout);
+  }, []); 
 
   return (
-    <>
-      <Loader id="loader_container"/>
-      <div id='main_content'>
-        <Navbar/>
+      <div>
+          {isLoading ? (
+              <div id="loader_container">
+                <Loader/>
+              </div>
+          ) : (
+              <div id="main_content">
+                <Navbar/>
+              </div>
+          )}
       </div>
-    </>
-  )
-}
+  );
+};
+
 
