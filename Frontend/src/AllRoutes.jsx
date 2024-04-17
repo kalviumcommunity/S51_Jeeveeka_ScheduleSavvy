063cd0064ec862b25ab750fd3c77c0a20ff5a7bb
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-04-17 15:06:54 +0530

    firebase initialising

--- Frontend/src/AllRoutes.jsx
+++ Frontend/src/AllRoutes.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom"
 import Home from './Components/Home'
 import Signup from './Components/Signup'
 import TitlePage from './Components/TitlePage'
+import Layout from './Components/Layout'
 
 export default function AllRoutes() {
   return (
