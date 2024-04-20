254b70d618ddda5ac094d6879a8ffdc9030c7e25
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-04-20 14:45:28 +0530

    added feedback form

---
+++ Frontend/src/Components/Feedback.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import '../Styles/Feedback.css';
+
+function Feedback() {
+  const [sent, setSent] = useState(false);
+
+  const addClass = () => {
+    setSent(true);
+  };
+
+  return (
+    <div className='feedback_form'>
+        <div id='back'><button className='feedback_button'>Back</button></div>
+        <div className={`wrapper centered ${sent ? 'sent' : ''}`}>
+        <article className="letter">
+            <div className="side">
+            <h1 className='feedback_title'>Feedback</h1>
+            <p className='feedback_text'>
+                <textarea className='feedback_textarea' placeholder="Your message"></textarea>
+            </p>
+            </div>
+            <div className="side">
+            <p className='feedback_text'>
+                <input className='feedback_input' type="text" placeholder="Your name" />
+            </p>
+            <p className='feedback_text'>
+                <input className='feedback_input' type="email" placeholder="Your email" />
+            </p>
+            <p className='feedback_text'>
+                <button className='feedback_button' id="sendLetter" onClick={addClass}>Send</button>
+            </p>
+            </div>
+        </article>
+        <div className="envelope front"></div>
+        <div className="envelope back"></div>
+        <p className="result-message centered">Thank you for your feedback!</p>
+        </div>
+    </div>
+  );
+}
+
+export default Feedback;
