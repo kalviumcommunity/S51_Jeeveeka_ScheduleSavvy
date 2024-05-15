4ba684d3586556dcd295a5ef41ed19f4911a3c14
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-05-15 10:57:18 +0530

    switching branch

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import Navbar from './Navbar'
 import Loader from './Loader'
 import Calender from './CalendarComp';
-import CrispChat from './Chat';
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -23,7 +21,6 @@ export default function Home() {
           ) : (
               <div id="main_content">
                 <Calender/>
-                <CrispChat/>
               </div>
           )}
       </div>
