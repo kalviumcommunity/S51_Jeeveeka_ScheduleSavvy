8846084b120d649de51b37490beea1be1abfea9a
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-05-23 10:14:02 +0530

    change

--- Frontend/src/AllRoutes.jsx
+++ Frontend/src/AllRoutes.jsx
@@ -14,6 +14,7 @@ import Pomodoro from './Components/Pomodoro'
 import ContactUs from './Components/ContactUs'
 import CrispChat from './Components/Chat'
 import Themes from './Components/Themes'
+import Chatbot from './Components/Chatbot'
 
 export default function AllRoutes() {
   return (
@@ -32,6 +33,7 @@ export default function AllRoutes() {
             <Route path='/contactus' element={<ContactUs/>}></Route>
             <Route path='/chat' element={<CrispChat/>}></Route>
             <Route path='/theme' element={<Themes/>}></Route>
+            <Route path='/chatbot' element={<Chatbot/>} ></Route>
         </Routes>
     </div>
   )
